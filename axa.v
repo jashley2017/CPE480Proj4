ec1fbce9e2586849b6dbb91cc4327b1239191b44
Isaac Rowe <irowe.bbn@gmail.com>
2019-11-02 18:09:31 -0400

    Add dump for top 16 registers of undo stack, number stages

--- axa.v
+++ axa.v
@@ -93,13 +93,13 @@ module processor(halt, reset, clk);
   integer undo_sp  = 0;
 
 
-  //BUFFER FIELDS FOR LOAD/DECODE OUTPUT, REGISTER READ INPUT
+  // BUFFER FIELDS FOR LOAD/DECODE OUTPUT, REGISTER READ INPUT
   reg `BUFOP op1;
   reg `BUFSRC src1;
   reg `BUFSRCTYPE srcType1;
   reg `BUFDADDR daddr1;
 
-  //BUFFER FIELDS FOR REGISTER READ OUTPUT, MEMORY READ/WRITE INPUT
+  // BUFFER FIELDS FOR REGISTER READ OUTPUT, MEMORY READ/WRITE INPUT
   reg `BUFOP op2;
   reg `BUFDADDR daddr2;
   reg `BUF16 srcFull2;
@@ -113,7 +113,7 @@ module processor(halt, reset, clk);
   reg `BUFSRCTYPE srcType3;
   reg `BUF16 destFull3;
 
-  //BUFFER FIELDS FOR ALU OUTPUT, REG WRITE INPUT
+  // BUFFER FIELDS FOR ALU OUTPUT, REG WRITE INPUT
   reg `BUFOP op4;
   reg `BUF16 result4;
   reg `BUFDADDR daddr4;
@@ -121,15 +121,18 @@ module processor(halt, reset, clk);
   reg is_zero;
   reg is_neg;
 
-  //BUFFER FIELDS FOR REG WRITE OUTPUT, LOAD/DECODE INPUT
+  // BUFFER FIELDS FOR REG WRITE OUTPUT, LOAD/DECODE INPUT
   reg  bjTaken;
   reg `BUFSRCTYPE bjSrcType;
   reg `BJ_TARGET bjTarget;
 
-  //RESET PROCEDURE
+  // RESET PROCEDURE
   always @(reset) begin
     halt = 0;
     pc = 0;
+    for(i = undo_sp; undo_sp - i < `NUMREGS; i = i - 1) begin
+      $dumpvars(0, undofile[i]);
+    end
     undo_sp = 0;
     $readmemh0(regfile);
     $readmemh1(datamem);
@@ -139,7 +142,7 @@ module processor(halt, reset, clk);
     end
   end
 
-  //LOAD AND DECODE STAGE
+  // STAGE 1: LOAD AND DECODE STAGE
   always @(posedge clk) begin
     if(!dataDependency & !undo_enable & !control_dependency) begin
         case (instmem[pc] `IMMSIZE)
@@ -174,7 +177,7 @@ module processor(halt, reset, clk);
     end
   end
 
-  //REGISTER READ
+  // STAGE 2: REGISTER READ
   always @(posedge clk) begin
     if(!dataDependency) begin
         destFull2 <= regfile[daddr1];
@@ -191,7 +194,7 @@ module processor(halt, reset, clk);
     end
   end
 
-  // MEMORY READ/WRITE
+  // STAGE 3: MEMORY READ/WRITE
   always @(posedge clk) begin
       if(!dataDependency) begin
           op3 <= op2;
@@ -210,7 +213,7 @@ module processor(halt, reset, clk);
       end
   end
 
-  //ALU
+  // STAGE 4: ALU
   always @(posedge clk) begin
     if(!undo_enable) begin
         // needed to store dest value in undobuff before write
@@ -255,8 +258,7 @@ module processor(halt, reset, clk);
     end
   end
 
-
-  //REGISTER WRITE
+  // STAGE 5: REGISTER WRITE
   always @(posedge clk) begin
     case (op4)
       `OPadd , `OPsub , `OPxor , `OPex  , `OProl , `OPshr , `OPor  , `OPand , `OPdup : begin
@@ -270,7 +272,7 @@ module processor(halt, reset, clk);
     bjSrcType <= srcType4;
   end
 
-  //UNDO STACK HANDLING
+  // UNDO STACK HANDLING
   always @(posedge clk) begin
     if (undo_enable == 1) begin
       if(|pushpop && undo_sp != 0) begin
@@ -285,6 +287,7 @@ module processor(halt, reset, clk);
     end
   end
 
+  // DATA DEPENDENCY HANDLING
   always @(posedge clk) begin
     //check for data dependencies after register read STAGE
     //only throw a data dependency if daddr matches or src matches a later daddr and src is a reg type
@@ -301,6 +304,7 @@ module processor(halt, reset, clk);
     else dataDependency <= 0;
     end
   end
+
 endmodule
 
 module testbench;
