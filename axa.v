8523496a9833aae681ab281d68dfdb91f3c88a63
Isaac Rowe <irowe.bbn@gmail.com>
2019-11-02 17:24:13 -0400

    Fixed typo op instead of op1

--- axa.v
+++ axa.v
@@ -154,7 +154,7 @@ module processor(halt, reset, clk);
 
         // Special case instructions: sys, land
         if(op1 == `OPsys) begin halt <= 1; end
-        if((op == `OPbjn) | (op == `OPbjnn) | (op == `OPbjz) | (op == `OPbjnz)) begin control_dependency <= 1; end
+        if((op1 == `OPbjn) | (op1 == `OPbjnn) | (op1 == `OPbjz) | (op1 == `OPbjnz)) begin control_dependency <= 1; end
         if(op1 == `OPland) begin to_push = lastPC; pushpop = 0; undo_enable <= 1; end
         lastPC <= pc;
 
